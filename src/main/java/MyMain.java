72db53a68da9a9a798096caea62c79f64fa3bfc6
Sentientnapkin <24svargas@athenian.org>
2021-09-15 14:30:41 -0700

    char methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,28 +4,36 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        if (ch>='A'&&ch<='Z')
+            return (char)(ch+35);
+        else return ch;
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not a lowercase letter,
     // it is returned unchanged.
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        if (ch>='a'&&ch<='z')
+            return (char)(ch-35);
+        else return ch;
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
+        String nonums = "";
+        for(int i = 0;i<str.length();i++){
+            if (str.charAt(i)<='0'||str.charAt(i)>='9')
+                nonums+=str.charAt(i);
+        }
+        return nonums;
     }
 
     // Write some code to test your methods!
     public static void main(String[] args) {
-        // ADD SOME TESTING CODE HERE
+        System.out.println(toLower('A')+"");
+        System.out.println(toUpper('j')+"");
+        System.out.println(removeNumbers("I <3 Kan5a5 4ev3r")+"");
     }
 }
